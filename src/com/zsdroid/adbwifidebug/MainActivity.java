abec223c99ac5e38f31399772a5e4162e7161c8b
zhusaidong <635925926@qq.com>
2015-10-18 17:43:33 +0800

    done code v1.0.0.0

--- src/com/zsdroid/adbwifidebug/MainActivity.java
+++ src/com/zsdroid/adbwifidebug/MainActivity.java
@@ -21,13 +21,21 @@ public class MainActivity extends Activity
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-		RootPermission(getPackageCodePath());
 		//初始化控件
 		button = (Button)findViewById(R.id.mainButton1);
 		edittext = (EditText)findViewById(R.id.mainEditText1);
+		
 		edittext.setCursorVisible(false);
 		edittext.setFocusable(false);
 		edittext.setFocusableInTouchMode(false);
+		//判断是否root
+		boolean root = IsRoot();
+		if(!root)
+		{
+			ShowToast("请先root，并重启应用！");
+			button.setEnabled(false);
+			return ;
+		}
 		//按钮事件
 		button.setOnClickListener(new OnClickListener(){
 				@Override
@@ -53,50 +61,30 @@ public class MainActivity extends Activity
 	public void StopAdbWifiDebug()
 	{
 		// TODO: Implement this method
-		ShowToast("stop");
 		if(status == true)
 		{
 			Command("stop adbd");
+			ShowToast("adb wifi 调试已停止");
 		}
 	}
 	//启动
 	public void StartAdbWifiDebug()
 	{
 		// TODO: Implement this method
-		ShowToast("start");
 		if(status == false)
 		{
 			String ip = GetIP();
-			ShowToast(ip);
+			if(ip.equals(""))
+			{
+				ShowToast("请先连接wifi!");
+				return ;
+			}
 			//执行开启命令
 			Command("setprop service.adb.tcp.port 5555;start adbd");
+			ShowToast("adb wifi 调试开启 \n  请在电脑端 输入 \n adb connect "+ip);
 		}
 	}
 	//执行命令
-	public void Cmd(String cmd)
-	{
-		Process pro;
-		try
-		{
-			 pro = Runtime.getRuntime().exec(cmd);
-			 InputStream is = pro.getInputStream();
-			 InputStreamReader isr = new InputStreamReader(is);
-			 BufferedReader br = new BufferedReader(isr);
-			 String line = "";
-			 StringBuilder sb = new StringBuilder(line);
-			 while((line = br.readLine()) != null)
-			 {
-				 sb.append(line);
-				 sb.append("\n");
-			 }
-			 String cmdtext = sb.toString();
-			 ShowToast(cmdtext);
-		}
-		catch (IOException e)
-		{
-			ShowToast("cmd="+e.getMessage());
-		}
-	}
 	public void Command(String cmd)
 	{
 		try
@@ -113,7 +101,6 @@ public class MainActivity extends Activity
 		}
 		catch (IOException e)
 		{
-			ShowToast("command="+e.getMessage());
 		}
 	}
 	/**
@@ -121,31 +108,24 @@ public class MainActivity extends Activity
 	 * 
 	 * @return 应用程序是/否获取Root权限
 	 */
-	public boolean RootPermission(String pkgCodePath) {
+	private boolean IsRoot(){
 		Process process = null;
-		DataOutputStream os = null;
-		try {
-			String cmd="chmod 777 " + pkgCodePath;
-			process = Runtime.getRuntime().exec("su"); //切换到root帐号
-			os = new DataOutputStream(process.getOutputStream());
-			os.writeBytes(cmd + "\n");
-			os.writeBytes("exit\n");
-			os.flush();
-			process.waitFor();
-		} catch (Exception e) {
-			ShowToast("upgradeRootPermission="+e.getMessage());
-			return false;
-		} finally {
-			try {
-				if (os != null) {
-					os.close();
-				}
-				process.destroy();
-			} catch (Exception e) {
-				ShowToast("upgradeRootPermission-finally"+e.getMessage());
+		try
+		{
+			process = Runtime.getRuntime().exec("su");
+			process.getOutputStream().write("exit\n".getBytes());
+			process.getOutputStream().flush();
+			int i = process.waitFor();
+			if(0 == i)
+			{
+				process = Runtime.getRuntime().exec("su");
+				return true;
 			}
+		} catch (Exception e)
+		{
+			return false;
 		}
-		return true;
+		return false;
 	}
 	//获取ip
 	public String GetIP()
@@ -153,9 +133,15 @@ public class MainActivity extends Activity
 		// TODO: Implement this method
 		WifiManager wm = (WifiManager) getApplicationContext().getSystemService(getApplicationContext().WIFI_SERVICE);
 		if(!wm.isWifiEnabled())
+		{
 			wm.setWifiEnabled(true);
+		}
 		WifiInfo wi = wm.getConnectionInfo();
 		int ip = wi.getIpAddress();
+		if(ip == 0)
+		{
+			return "";
+		}
 		return IntToIp(ip);
 	}
 	//int转ip
@@ -163,13 +149,12 @@ public class MainActivity extends Activity
 	{
 		return (i & 0xFF ) + "." + ((i >> 8 ) & 0xFF) + "." + ((i >> 16 ) & 0xFF) + "." + ( i >> 24 & 0xFF) ;
 	}
-	//显示日志
+	//显示
 	public void ShowToast(String toast)
 	{
 		if(!toast.equals(""))
 		{
 			edittext.setText(edittext.getText()+"\n============\n>> "+toast);
-			//Toast.makeText(getApplicationContext(),toast,Toast.LENGTH_SHORT);
 		}
 	}
 	public void ShowToast(int toast)
